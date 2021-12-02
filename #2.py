f961c4fe44607d61e6c14f4c89083c282c90eac2
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-12-01 22:11:42 -0600

    Update #2.py

--- #2.py
+++ #2.py
@@ -10,3 +10,21 @@ if G + I < O:
     print('number is less than 10')
 if G + I == O:
     print('the number is equal to 10')
+
+'''    
+Same as Pb#1
+Define function: sum of two inputs are compared to 10
+Get input
+Call function with passing the two inputs.
+'''
+def numbercomparsion(a, b):
+   if a + b > 10:
+        print("sum of {} and {} is greater than 10. ".format(a, b))
+   elif a + b < 10:
+        print("sum of {} and {} is less than 10. ".format(a, b))
+   else:
+        print("sum of {} and {} is equal to 10. ".format(a, b))
+
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+numbercomparison(a,b)
