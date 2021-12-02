972f568692f27bbc3338e10abf1dba3f564334ad
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-12-01 22:13:09 -0600

    Update #3.py

--- #3.py
+++ #3.py
@@ -5,3 +5,19 @@ list = [1, 2, 3, 4, 5,]
 for item in list:
     if item < 5 :
         print(item)
+        
+'''
+input: list
+define function to check whether there is 5 in the list
+call function
+'''
+def checkValue(list):
+    value  = 5
+    if value in list:
+        print('five is in the list')
+        print(list)
+    else:
+        print('five is not the list')
+
+list = [2, 3, 5, 1]
+checkValue(list)
